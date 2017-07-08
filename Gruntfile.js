223828799362ae4c23d759400084c7b6eacad768
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-07-08 19:59:45 -0300

    versionamento de arquivos estáticos com rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,12 +31,29 @@ module.exports = function(grunt) {
                 src: '**/*.{png,jpg,gif}',
                 dest: 'dist/img'
             }
+        },
+
+        rev: {
+            options:{
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+
+            imagens:{
+               src: ['dist/img/**/*.{png,jpg,gif}']
+            },
+
+            minificados: {
+                src: ['dist/js/**/*.min.js', 'dist/css/**/*.min.css']
+            }
         }
+
     });
 
     //registrando task para minificação
     grunt.registerTask('dist', ['clean', 'copy']);
-    grunt.registerTask('minifica', ['useminPrepare','concat', 'uglify', 'cssmin', 'usemin', 'imagemin']);
+    grunt.registerTask('minifica', ['useminPrepare','concat', 'uglify', 'cssmin','rev:imagens', 'rev:minificados', 'usemin', 'imagemin']);
     //registrando tasks
     grunt.registerTask( 'default', ['dist', 'minifica']);
     
@@ -48,4 +65,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-rev');
 };
