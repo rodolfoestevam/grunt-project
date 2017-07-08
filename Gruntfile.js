c1d505bcf133155438d22417ba9625f49c9e09b0
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-07-08 19:48:15 -0300

    minificacao e concatenacao, otimizacao de imagens

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+/*recebe o objeto grunt como parametro*/
+module.exports = function(grunt) {
+    grunt.initConfig({
+        //tasks
+        copy: {
+            public: {
+                cwd: 'public',
+                src: '**',
+                dest: 'dist',
+                expand: true
+            }
+        },
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        },
+
+        useminPrepare: {
+            html: 'dist/**/*.html'
+        },
+
+        usemin: {
+            html: 'dist/**/*.html' 
+        },
+
+        imagemin:{
+            public:{
+                expand: true,
+                cwd: 'dist/img',
+                src: '**/*.{png,jpg,gif}',
+                dest: 'dist/img'
+            }
+        }
+    });
+
+    //registrando task para minificação
+    grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('minifica', ['useminPrepare','concat', 'uglify', 'cssmin', 'usemin', 'imagemin']);
+    //registrando tasks
+    grunt.registerTask( 'default', ['dist', 'minifica']);
+    
+    //carregando Tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+};
